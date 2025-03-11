472585a5ea7919471268178d9d90c4b0e182c3bd
M-OMER-DEV <m.omer.dev17@gmail.com>
2025-03-11 16:11:24 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,9 +66,9 @@
 
                 <nav class="s-header__nav">
                     <ul class="s-header__menu-links">
-                        <li class="current"><a class="smoothscroll" href="#intro">Intro</a></li>
+                        <li class="current"><a class="smoothscroll" href="#intro">Home</a></li>
                         <li><a class="smoothscroll" href="#about">About</a></li>
-                        <li><a class="smoothscroll" href="#works">Works</a></li>
+                        <li><a class="smoothscroll" href="#works">Work</a></li>
                         <li><a class="smoothscroll" href="#footer">Contact</a></li>
                     </ul> <!-- s-header__menu-links -->
                 </nav> <!-- end s-header__nav -->
